54b8116211d35c42e7db740bd331c13ca9b78767
Armyja <546323405@qq,com>
2022-04-15 18:42:26 +0800

    21560

---
+++ 1523.在区间范围内统计奇数数目.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1523 lang=rust
+ *
+ * [1523] 在区间范围内统计奇数数目
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn count_odds(low: i32, high: i32) -> i32 {
+
+    }
+}
+// @lc code=end
+
